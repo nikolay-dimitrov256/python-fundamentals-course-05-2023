312350e88991d6b1cda2667e404b98cc09848fe1
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2023-07-31 21:51:08 +0300

    Update strange_zoo.py

--- fun_lists_basics_lab/strange_zoo.py
+++ fun_lists_basics_lab/strange_zoo.py
@@ -1,13 +1,3 @@
-# tail, body, head = input(), input(), input()
-#
-# body_parts = list()
-#
-# body_parts.append(head)
-# body_parts.append(body)
-# body_parts.append(tail)
-#
-# print(body_parts)
-
 tail, body, head = input(), input(), input()
 
 body_parts = [tail, body, head]
