a7d0892e05fbb650b4985de5b38a9f5739661049
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2023-07-31 22:05:13 +0300

    Update palindrome_integers.py

--- fun_funcitions_exercise/palindrome_integers.py
+++ fun_funcitions_exercise/palindrome_integers.py
@@ -1,14 +1,3 @@
-# def is_palindrome(some_list: list):
-#     for number in some_list:
-#         if number == number[::-1]:
-#             print(True)
-#         else:
-#             print(False)
-#
-#
-# list_numbers = input().split(", ")
-# is_palindrome(list_numbers)
-
 def is_palindrome(some_number: str) -> bool:
     if some_number == some_number[::-1]:
         return True
