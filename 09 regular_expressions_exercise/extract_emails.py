8f799ea134d8c0fe5a7193cf1791176b2b9249bf
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2023-08-02 22:29:11 +0300

    Update extract_emails.py

--- 09 regular_expressions_exercise/extract_emails.py
+++ 09 regular_expressions_exercise/extract_emails.py
@@ -2,7 +2,6 @@ import re
 
 text = input()
 pattern = r'\s(([a-z\d][a-z\d\.\-_]*)@([a-z][a-z\-]*)(\.[a-z]+)+)\b'
-# pattern = r"\s(([a-z0-9]+[a-z0-9\.\-\_]*)@([a-z\-]+)(\.[a-z]+)+)\b"
 
 matches = re.findall(pattern, text)
 
