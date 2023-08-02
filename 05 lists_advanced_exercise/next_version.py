f016d93458e379ebd8d4b68b8fe74a2aec7c1866
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2023-08-02 21:56:28 +0300

    Update next_version.py

--- 05 lists_advanced_exercise/next_version.py
+++ 05 lists_advanced_exercise/next_version.py
@@ -1,14 +1,3 @@
-# version_number = [num for num in input().split(".")]
-# new_version_int = int("".join(version_number)) + 1
-# new_version_list = []
-#
-# for num in str(new_version_int):
-#     new_version_list.append(str(num))
-#
-# final_string = ".".join(new_version_list)
-#
-# print(final_string)
-
 version_number = [int(num) for num in input().split(".")]
 
 version_number[-1] += 1
